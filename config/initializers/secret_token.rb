4e9096370ab6bca1cc8e0299ca52223ab3248819
miio <info@miio.info>
2011-12-14 00:19:00 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signspot::Application.config.secret_token = '7fe0396a05f15484c32fad5aaa10be9911ec9bf0b2a2db8a3bce894c063601e2d1b40892512108ab9543bdc00458084fe6d06adbf192a57cc359489a21098e5c'
